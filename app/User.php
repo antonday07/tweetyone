b906196891c254172d20db0accadba43d558b2e3
AntonDev07 <anhquangtong2000@gmail.com>
2021-02-18 19:00:37 +0700

    add check null

--- app/User.php
+++ app/User.php
@@ -61,6 +61,7 @@ class User extends Authenticatable
         $friends = $this->follows()->pluck('id');
         $tweets = Tweet::whereIn('user_id', $friends)
             ->orWhere('user_id', $this->id)
+            ->withLikes()
             ->latest()
             ->get();
 
